5fb20e9c2d9052f67ac3c592f1ad575b00aa79e2
maaonan <xiaozhiqiu13@sina.com>
2018-09-28 13:57:49 +0800

    Use list not tuple.

--- juedge_day.py
+++ juedge_day.py
@@ -8,6 +8,13 @@
 from _datetime import datetime
 
 
+def is_leap_year(year):
+    is_leap = False
+    if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+        is_leap = True
+    return is_leap
+
+
 def main():
     input_date_str = input('请输入日期(yyyy/mm/dd)：\n')
     input_date = datetime.strptime(input_date_str, '%Y/%m/%d')
@@ -18,14 +25,12 @@ def main():
     day = input_date.day
 
     # 计算之前每月天数总和以及当前月份天数（假设为平年）
-    days_in_month_tup = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
-    days = sum(days_in_month_tup[: month - 1]) + day
+    days_in_month_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap_year(year):
+        days_in_month_list[1] = 29
+    days = sum(days_in_month_list[: month - 1]) + day
 
-    if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
-        if month > 2:
-            days += 1
-
-    print("这是第{}天".format(days))
+    print("这是{}年的第{}天".format(year, days))
 
 
 if __name__ == '__main__':
